f01389f4404375806b2dbc41fb4d18ba00e27e75
Aadil <aadilkhan3007@gmail.com>
2025-01-14 23:15:04 +0530

    Dom-project-1-completed

---
+++ index.js
@@ -0,0 +1,9 @@
+let divObj = document.getElementById("div");
+divObj.style.backgroundColor = "yellow";
+divObj.style.margin = "20px";
+divObj.style.padding = "10px";
+divObj.style.fontSize = "18px";
+divObj.style.fontWeight = "bold";
+divObj.style.height = "200px";
+divObj.style.width = "300px";
+divObj.style.color = "blue";
